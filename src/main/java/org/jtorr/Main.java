92458bd25a3dd460d8ea7bbcb95a17117a5b57eb
taulin-a <ribx.code@gmail.com>
2024-01-19 19:46:57 -0300

    Starting prototype by getting .torrent file and converting to dictionary

--- src/main/java/org/jtorr/Main.java
+++ src/main/java/org/jtorr/Main.java
@@ -1,7 +1,23 @@
 package org.jtorr;
 
+import com.dampcake.bencode.Bencode;
+import com.dampcake.bencode.Type;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            var torrFileByteArr = Files.readAllBytes(Path.of("/home/paulo_alves/Downloads/ebook.torrent"));
+            var bencode = new Bencode();
+            Map<String, Object> dict = bencode.decode(torrFileByteArr, Type.DICTIONARY);
+
+            System.out.println(dict);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
